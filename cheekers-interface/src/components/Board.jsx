da2f02d0f4937fd770cd0c57311c76bd617f6595
Trevor DePew <t.depewmusic@gmail.com>
2023-01-03 12:02:53 -0500

    adding tile obects to the board elements

--- cheekers-interface/src/components/Board.jsx
+++ cheekers-interface/src/components/Board.jsx
@@ -1,9 +1,9 @@
 import Square from "./Square"
 import Piece from "./Piece"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 const Board = () => {
-  const squares = [1, 2, 3, 4, 5, 6, 7, 8] //An array (squares) which is mapped over to create columns. 
+  const squares = [1, 2, 3, 4] //An array (squares) which is mapped over to create columns. 
   // const [occupied, setOccupied] = useState(false) //A state variable set to false in order to represent whether a square is occupied
   // const handleClick = (e) => { //Used to set whether a square is occupied or not
   //   setOccupied(!occupied)
@@ -13,11 +13,22 @@ const Board = () => {
   //     </>
   //   )
   // }
+  const [tiles, setTiles] = useState([])
+  let num = 0
+
+  useEffect(() =>{
+    const request = async () => {
+      let req = await fetch("http://localhost:3000/tiles")
+      let res = await req.json()
+      setTiles(res)
+    }
+    request()
+  }, [])
   
   return (
     <div className="play-board">
       {
-        squares.map((yCoor, i) => {
+        squares.map((yCoor, y) => {
           const columnId = `col-${yCoor}` //Each column is given an id using the numbers in the squares array 
           return(
             <div //Then each column returns a div where the squares array is mapped over again for each column 
@@ -25,11 +36,12 @@ const Board = () => {
             id={columnId}
             >
               {
-                squares.map((xCoor, j) => { //The second map generates the squares and x-coordinates which are combined with the column numbers to create the coordinates 
+                squares.map((xCoor, x) => { //The second map generates the squares and x-coordinates which are combined with the column numbers to create the coordinates 
                   
                   return(
                     <>
-                      <Square xAxis={xCoor} yAxis={yCoor}/> 
+                      <Square xAxis={xCoor} yAxis={yCoor} tiles={tiles} x={x} y={y} num={num}/> 
+                      {num += 1}
                     </>
                   )
                 })
@@ -38,7 +50,7 @@ const Board = () => {
           )
         })
       }
-      <Piece />
+      {/* <Piece /> */}
     </div>
   )
 
--- cheekers-interface/src/components/Square.jsx
+++ cheekers-interface/src/components/Square.jsx
@@ -1,18 +1,17 @@
 import { useState } from 'react'
 
-const Square = (xAxis, yAxis) => {
+const Square = ({xAxis, yAxis, tiles, x, y, num}) => {
   const [occupied, setOccupied] = useState(false)
-  let coors = xAxis
-  let coordinates = `(${coors.xAxis}, ${coors.yAxis})`
-  
+
+  let coordinates = `(${xAxis}, ${yAxis})`
+  // console.log(`(${x},${y})`)
 
   const handleClick = (e) => {
     setOccupied(!occupied)
   }
-  return (
+  return ( 
     
     <div
-      
       className="square"
       id={coordinates} //The id of each square = the coordinate variable 
       occupied={!occupied ? "false" : "true"} //Sets an occupied property for the element
@@ -20,16 +19,18 @@ const Square = (xAxis, yAxis) => {
       style={{
         background: (
           (
-            (coors.yAxis % 2 > 0) ? //If y is odd
-              (((coors.yAxis % 2 > 0) && (coors.xAxis % 2 == 0)) ? //If y is odd and x is even
+            (yAxis % 2 > 0) ? //If y is odd
+              (((yAxis % 2 > 0) && (xAxis % 2 == 0)) ? //If y is odd and x is even
                 "black" : "tan") :
-              ((coors.yAxis % 2 == 0) && (coors.xAxis % 2 > 0)) ? //If y is even and x is odd
+              ((yAxis % 2 == 0) && (xAxis % 2 > 0)) ? //If y is even and x is odd
                 "black" : "tan"
           )
         )
       }}
       
-    ></div>
+    >
+      {console.log(tiles[num])}
+    </div>
   )
 }
 
