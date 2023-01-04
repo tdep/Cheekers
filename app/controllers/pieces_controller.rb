5cccbbcbd0c7c2577b7820557b2c87f4fafc14b4
Trevor DePew <t.depewmusic@gmail.com>
2023-01-04 04:10:30 -0500

    didn't change anything

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -3,4 +3,10 @@ class PiecesController < ApplicationController
   def index
     render json: Piece.all
   end
+
+  def update
+    piece = Piece.find_by!(id: params[:id])
+    piece.update(selected: params[:selected])
+    render json: piece
+  end
 end
