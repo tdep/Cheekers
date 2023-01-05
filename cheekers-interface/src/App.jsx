9230441ab12561f5e12e72a037b269116569d5f2
Cooper Hall <cooperhall111@gmail.com>
2023-01-05 16:00:16 -0500

    added players independent pages

--- cheekers-interface/src/App.jsx
+++ cheekers-interface/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import React from "react"
 import BattlePage from './components/BattlePage'
 import StartPage from './components/StartPage'
+import PlayerOnePage from './components/PlayerOnePage'
+import PlayerTwoPage from './components/PlayerTwoPage'
 import PlayerPage from './components/PlayerPage'
 import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
@@ -16,7 +18,10 @@ function App() {
     <BrowserRouter>
       <Routes>
           <Route path="/" element={<StartPage />}/>
-          <Route path="players" element={<PlayerPage setPlayerOne={setPlayerOne} setPlayerTwo={setPlayerTwo}/>}/>
+          <Route path="playerone" element={<PlayerOnePage setPlayerOne={setPlayerOne} />}/>
+          <Route path="playertwo" element={<PlayerTwoPage setPlayerTwo={setPlayerTwo}/>}/>
+          
+
           <Route path="players/game" element={<BattlePage playerOne={playerOne} playerTwo={playerTwo} />}/>
       </Routes>
     </BrowserRouter>
---
+++ cheekers-interface/src/components/PlayerTwoPage.jsx
@@ -0,0 +1,10 @@
+import {Link} from "react-router-dom"
+
+const PlayerTwoPage = ({setPlayerTwo}) => {
+  return(
+    <div>
+      player2
+    </div>
+  )
+}
+export default PlayerTwoPage
